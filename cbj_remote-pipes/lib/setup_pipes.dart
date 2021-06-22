52c6b025d12a40a3ef6f1828ff0d35f3bdd56b2c
Guy Luz <guyluz11@gmail.com>
2021-06-22 09:37:16 +0300

    Add lint rules and license.

--- cbj_remote-pipes/lib/setup_pipes.dart
+++ cbj_remote-pipes/lib/setup_pipes.dart
@@ -2,7 +2,7 @@ import 'package:cbj_remote_pipes/domain/hube_server/smart_server_u.dart';
 
 class SetupPipes {
   Future<void> main() async {
-    SmartServerU smartServerU = SmartServerU();
+    final SmartServerU smartServerU = SmartServerU();
     smartServerU.startLocalServer();
   }
 }
